df112a2ea50b9be0f1da94fc14d9317525de9918
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-03-15 17:12:07 +0530

    Create restart.py

---
+++ WebStreamer/bot/plugins/restart.py
@@ -0,0 +1,21 @@
+import asyncio
+import io
+import logging
+import os
+import shutil
+import sys
+import time
+import traceback
+
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from WebStreamer.bot import StreamBot
+from WebStreamer.vars import Var
+from WebStreamer.__main__ import * # yea 
+from logging.handlers import RotatingFileHandler
+
+@StreamBot.on_message(filters.command(["restart", "restarts"]))
+async def restart_bot(message):
+    await message.reply("Bot will be restarted...")
+    args = [sys.executable, "-m", "WebStreamer"]
+    os.execl(sys.executable, *args)
